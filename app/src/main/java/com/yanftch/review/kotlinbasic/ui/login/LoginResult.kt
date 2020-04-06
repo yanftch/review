424eca2f06fb99afa59b3f9dc0281c29b1ee640f
qiuyf1 <qiuyf1@ziroom.com>
2020-04-06 19:05:53 +0800

    remove

--- app/src/main/java/com/yanftch/review/kotlinbasic/ui/login/LoginResult.kt
+++
@@ -1,9 +0,0 @@
-package com.yanftch.review.kotlinbasic.ui.login
-
-/**
- * Authentication result : success (user details) or error message.
- */
-data class LoginResult(
-    val success: LoggedInUserView? = null,
-    val error: Int? = null
-)
